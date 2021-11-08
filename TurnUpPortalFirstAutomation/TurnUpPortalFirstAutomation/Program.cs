706d812b03f6c7c84cfb5539747e1263ee53ab05
Shalan <shalanqa2011@gmail.com>
2021-11-08 14:08:51 +1300

    Added assertions for Delete funtionality

--- TurnUpPortalFirstAutomation/TurnUpPortalFirstAutomation/Program.cs
+++ TurnUpPortalFirstAutomation/TurnUpPortalFirstAutomation/Program.cs
@@ -37,39 +37,50 @@ namespace TurnUpPortalFirstAutomation
             //click on Administration tab
             IWebElement adminTab = driver.FindElement(By.XPath("//a[@class='dropdown-toggle']"));
             adminTab.Click();
+
             //Select time&material option from dropdown
             IWebElement timeMaterialOption = driver.FindElement(By.XPath("//a[contains(@href,'Material')]"));
             timeMaterialOption.Click();
+
             //Click on CreateNew button
             IWebElement createNewButton = driver.FindElement(By.XPath("//a[@class='btn btn-primary']"));
             createNewButton.Click();
+
             // Click on typeCode 
             IWebElement typeCode = driver.FindElement(By.XPath("(//span[@class='k-icon k-i-arrow-s'])[1]"));
             typeCode.Click();
             Thread.Sleep(2000);
+
             // select time option from typeCode dropdown
             IWebElement timeOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[2]"));
                 timeOption.Click();
+
             //Enter code in codeTextBox
             IWebElement codeTextBox = driver.FindElement(By.XPath("//input[@id='Code']"));
             codeTextBox.SendKeys("TurnUpPortal 2021");
+
             // enter description in descriptionTextBox
             IWebElement descriptionTextBox = driver.FindElement(By.XPath("//input[@id='Description']"));
             descriptionTextBox.SendKeys("TurnUpPortal 2021");
+
             //enter price in pricePerUnitTextBox
             
             IWebElement pricePerUnitTextBox = driver.FindElement(By.XPath("//input[@class='k-formatted-value k-input']"));
             //pricePerUnitTextBox.Click();
             pricePerUnitTextBox.SendKeys("25");
+
             // Click on SaveButton
             IWebElement saveButton = driver.FindElement(By.XPath("//input[@id='SaveButton']"));
             saveButton.Click();
             Thread.Sleep(2000);
+
             //Click on lastPage of the table
             IWebElement lastPageButton = driver.FindElement(By.XPath("//span[@class='k-icon k-i-seek-e']"));
             lastPageButton.Click();
+
             // Validate if the record has been created and has the expected values.
             IWebElement newRecord = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+            
             if(newRecord.Text == "TurnUpPortal 2021")
             {
                 Console.WriteLine("New Record has been created successfully, hence test case is passed");
@@ -87,20 +98,20 @@ namespace TurnUpPortalFirstAutomation
             editButton.Click();
             Thread.Sleep(2000);
 
-            //clear the previous input from codeTextBox
-                IWebElement coTextBox = driver.FindElement(By.Id("Code"));
-                 coTextBox.Clear();
+            //clear the previous input from codeTextBox (Code repeated here)
+            IWebElement coTextBox = driver.FindElement(By.Id("Code"));
+            coTextBox.Clear();
 
-            // Change the codeTextBox input to ShaAutomation22
+            // Change the codeTextBox input to ShaAutomation22 (Code repeated here)
             coTextBox.SendKeys("ShaAutomation22");
 
 
-            //Click on saveButton
+            //Click on saveButton (Code repeated here)
             IWebElement saveButtons = driver.FindElement(By.Id("SaveButton"));
             saveButtons.Click();
             Thread.Sleep(2000);
 
-            // Click on last page of the table again
+            // Click on last page of the table again (Code repated here
             IWebElement lastPaButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
             lastPaButton.Click();
 
@@ -112,10 +123,22 @@ namespace TurnUpPortalFirstAutomation
 
             driver.SwitchTo().Alert().Accept();
 
+            //Click on last page of the table again
+            Thread.Sleep(2000);
 
+           
+           IWebElement tableText = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
 
+            if(tableText.Text == "ShaAutomation22")
+            {
+                Console.WriteLine("New record is still not deleted hence test case is failed");
+            }
+            else
+            {
+                Console.WriteLine("New record has been successfully deleted, hence test case is passed");
+            }
 
-
+            
 
 
 
